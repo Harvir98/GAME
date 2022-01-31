16f5b289ebaa938491b36f1e5d93ab0a45e812bb
harvir_g1@icloud.com <harvir_g1@icloud.com>
2022-01-31 22:03:26 +0000

    added all basic html and some cscss styling

---
+++ app.js
@@ -0,0 +1,12 @@
+// <!-- create database for words in js 
+// event listener on  -->
+
+// <!-- create mobile version for tomororw  -->
+
+// <!-- img for each section for the drawing  -->
+// random word is generated from (database?)
+// Once I click on a button it should go a different color to highlight that its already been selected 
+// when a letter is incorrectly entered a new image will show
+// If the letter clicked is correct the letter will show on the correct line
+
+// when you guess the word a pop up saying you won will display 
