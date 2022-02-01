0f8b6377887514b0d3c56bea9ccbb4dd2c017b78
harvir_g1@icloud.com <harvir_g1@icloud.com>
2022-02-01 22:01:20 +0000

    added left and up in js with some styling

---
+++ gamepsuedo.js
@@ -0,0 +1,26 @@
+// SNAKE PSEUDO CODE 
+
+// One big box seperated into smaller boxes (use grid or table)
+// Snake is a html element?
+  // Created by changing background color of the square
+    // Class to every div
+// Snake has set starting position 
+// piece of good food and bad food is randomly generated within the box 
+  // bad food will end the game 
+
+// Event Listener
+  // Use arrow keys to move the snake. Everytime a arrow key is pressed the snake changes direction depending on the arrow key pressed.
+
+// score that changes everytime you eat a piece of good food 
+// IF the snake hits the border = game over 
+  // p tag that will display to signal the game is over
+
+// Advanced 
+  // IF the snake eats good food its length will increase 
+  // IF the snake bumps into itself, the game will be over
+
+
+// Set intervals for moving snake
+
+// at set intervals add a class that will change the color of a box 
+// create function then insert  it into setIntervals function 
