56e79b395ed070aa65fa7ecef19097d3a0d45529
harvir_g1@icloud.com <harvir_g1@icloud.com>
2022-02-02 21:21:28 +0000

    fixed some errors with arrow key inputs

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 let total = 1;
-let total2 = 99;
 let movingRight = false;
 let movingDown = false;
 let movingLeft = false;
-let movingUp = true; 
+let movingUp = false;
 
 
 const movingBoxDown = () => {
   if (movingDown) {
-  if (total >=10) {
-    document.getElementById(`box-${total-10}`).classList.remove("active");
-  }
-  document.getElementById(`box-${total}`).classList.add("active");
-  total += 10;
-} else return;
+    if (total >=10) {
+      document.getElementById(`box-${total-10}`).classList.remove("active");
+      // document.getElementById(`box-${total-11}`).classList.remove("active"); Trouble with box-1 going down
+    }
+    document.getElementById(`box-${total}`).classList.add("active");
+    total += 10;
+    } else return;
 } 
 
 // setInterval(movingBoxDown, 1000);
@@ -26,95 +26,38 @@ const movingBoxRight = () => {
     document.getElementById(`box-${total}`).classList.add("active");
     total += 1;
   } else return;
-  // if (total >= 2) {
-  //   document.getElementById(`box-${total-1}`).classList.remove("active");
-  // }
-  // document.getElementById(`box-${total}`).classList.add("active");
-  // total += 1;
 } 
 
 // setInterval(movingBoxRight, 1000);
 
 const movingBoxLeft = () => {
   if (movingLeft) {
-  if (total2 >= 1) {
-    document.getElementById(`box-${total2+1}`).classList.remove("active");
+  if (total >= 2) {
+    document.getElementById(`box-${total+1}`).classList.remove("active");
   }
-  document.getElementById(`box-${total2}`).classList.add("active");
-  total2 -= 1;
+  document.getElementById(`box-${total}`).classList.add("active");
+  total = total - 1;
 } else return;
 } 
 
 const movingBoxUp = () => {
   if (movingUp) {
-  if (total2 >= 1) {
-    document.getElementById(`box-${total2+1}`).classList.remove("active");
+  if (total >= 10) {
+    document.getElementById(`box-${total+10}`).classList.remove("active");
+    document.getElementById(`box-${total+11}`).classList.remove("active");
   }
-  document.getElementById(`box-${total2}`).classList.add("active");
-  total2 -= 1;
+  document.getElementById(`box-${total}`).classList.add("active");
+  total -= 10;
 } else return;
 } 
 // setInterval(movingBoxLeft, 1000);
-
-
-// Detecting an arrow key in document 
-
-// document.addEventListener("keydown", moveLeft);
-
-// // // es5 syntax, how to convert to es6
-
-// function moveLeft (event){
-//   if (event.keyCode == 37) {
-//     console.log("LEFT")
-//   }
-// }
-
-// document.addEventListener("keydown", moveUp);
-
-// function moveUp (event){
-//   if (event.keyCode == 38) {
-//     console.log("UP")
-//   }
-// }
-
-// document.addEventListener("keydown", moveDown);
-
-// function moveDown (event){
-//   if (event.keyCode == 40) {
-//     if (total >=10) {
-//       document.getElementById(`box-${total-10}`).classList.remove("active");
-//   }
-//   document.getElementById(`box-${total}`).classList.add("active");
-//   total += 10;
-
-//   }
-
-//   if (event.keyCode == 39) {
-//     if (total >= 2) {
-//           document.getElementById(`box-${total-1}`).classList.remove("active");
-//         }
-//         document.getElementById(`box-${total}`).classList.add("active");
-//         total += 1;
-//       } 
-  
-//   if (event.keyCode == 38) {    
-//     if (total2 >= 1) {
-//           document.getElementById(`box-${total2+1}`).classList.remove("active");
-//         }
-//         document.getElementById(`box-${total2}`).classList.add("active");
-//         total2 -= 1;
-//   }  
-// }
-   
-// setInterval(moveDown, 1000);
-
-
 
 const moveRight = (event) => {
   if (event.keyCode == 39) {
     movingRight = true;
     movingLeft = false;
     movingDown = false;
+    movingUp = false; 
     setInterval(movingBoxRight, 1000);
   }
 }
@@ -124,12 +67,11 @@ const moveDown = (event) => {
     movingRight = false;
     movingDown = true;
     movingLeft = false;
+    movingUp = false; 
+
     setInterval(movingBoxDown, 1000);
   }
 }
-
-
-
 
 // Moving Left is not working becuase i have set total2 too 99 like a idiot but how to fix it ???
 
@@ -138,12 +80,22 @@ const moveLeft = (event) => {
     movingRight = false;
     movingLeft = true;
     movingDown = false;
+    movingUp = false; 
     setInterval(movingBoxLeft, 1000);
   }
 }
 
-
-
+const moveUp = (event) => {
+  if (event.keyCode == 38) {
+    movingRight = false;
+    movingLeft = false;
+    movingDown = false;
+    movingUp = true; 
+    setInterval(movingBoxUp, 1000);
+  }
+}
+ 
+// GREEEEEN
 
 document.addEventListener("keydown", moveRight);
 document.addEventListener("keydown", moveDown);
