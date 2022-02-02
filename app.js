2b9dd7539d01586759fc4623f7f9db7ab9ca0b66
harvir_g1@icloud.com <harvir_g1@icloud.com>
2022-02-02 16:03:26 +0000

    added key functions to move left and right

--- app.js
+++ app.js
@@ -1,49 +1,148 @@
 let total = 1;
+let total2 = 99;
+let movingRight = false;
+let movingDown = false;
+let movingLeft = false;
 
 
-// const movingBoxDown = () => {
-//   if (total >=10) {
-//     document.getElementById(`box-${total-10}`).classList.remove("active");
-//   }
-//   document.getElementById(`box-${total}`).classList.add("active");
-//   total += 10;
-// } 
+const movingBoxDown = () => {
+  if (movingDown) {
+  if (total >=10) {
+    document.getElementById(`box-${total-10}`).classList.remove("active");
+  }
+  document.getElementById(`box-${total}`).classList.add("active");
+  total += 10;
+} else return;
+} 
 
 // setInterval(movingBoxDown, 1000);
 
-// const movingBoxRight = () => {
-//   if (total >= 2) {
-//     document.getElementById(`box-${total-1}`).classList.remove("active");
-//   }
-//   document.getElementById(`box-${total}`).classList.add("active");
-//   total += 1;
-// // } 
+const movingBoxRight = () => {
+  if (movingRight) {
+    if (total >= 2) {
+      document.getElementById(`box-${total-1}`).classList.remove("active");
+    }
+    document.getElementById(`box-${total}`).classList.add("active");
+    total += 1;
+  } else return;
+  // if (total >= 2) {
+  //   document.getElementById(`box-${total-1}`).classList.remove("active");
+  // }
+  // document.getElementById(`box-${total}`).classList.add("active");
+  // total += 1;
+} 
 
 // setInterval(movingBoxRight, 1000);
 
-let total2 = 99;
-
 const movingBoxLeft = () => {
+  if (movingLeft) {
   if (total2 >= 1) {
     document.getElementById(`box-${total2+1}`).classList.remove("active");
   }
   document.getElementById(`box-${total2}`).classList.add("active");
   total2 -= 1;
+} else return;
 } 
 
-setInterval(movingBoxLeft, 1000);
+// setInterval(movingBoxLeft, 1000);
+
+
+// Detecting an arrow key in document 
+
+// document.addEventListener("keydown", moveLeft);
+
+// // // es5 syntax, how to convert to es6
+
+// function moveLeft (event){
+//   if (event.keyCode == 37) {
+//     console.log("LEFT")
+//   }
+// }
+
+// document.addEventListener("keydown", moveUp);
+
+// function moveUp (event){
+//   if (event.keyCode == 38) {
+//     console.log("UP")
+//   }
+// }
+
+// document.addEventListener("keydown", moveDown);
+
+// function moveDown (event){
+//   if (event.keyCode == 40) {
+//     if (total >=10) {
+//       document.getElementById(`box-${total-10}`).classList.remove("active");
+//   }
+//   document.getElementById(`box-${total}`).classList.add("active");
+//   total += 10;
+
+//   }
+
+//   if (event.keyCode == 39) {
+//     if (total >= 2) {
+//           document.getElementById(`box-${total-1}`).classList.remove("active");
+//         }
+//         document.getElementById(`box-${total}`).classList.add("active");
+//         total += 1;
+//       } 
+  
+//   if (event.keyCode == 38) {    
+//     if (total2 >= 1) {
+//           document.getElementById(`box-${total2+1}`).classList.remove("active");
+//         }
+//         document.getElementById(`box-${total2}`).classList.add("active");
+//         total2 -= 1;
+//   }  
+// }
+   
+// setInterval(moveDown, 1000);
 
 
 
-document.addEventListener("keydown", move);
-
-// es5 syntax
-
-function move (event){
-  if (event.keyCode == 37) {
-    console.log("LEFT")
+const moveRight = (event) => {
+  
+  if (event.keyCode == 39) {
+    movingRight = true;
+    movingLeft = false;
+    movingDown = false;
+    setInterval(movingBoxRight, 1000);
   }
 }
+
+const moveDown = (event) => {
+  
+  if (event.keyCode == 40) {
+    movingRight = false;
+    movingDown = true;
+    movingLeft = false;
+    setInterval(movingBoxDown, 1000);
+  }
+}
+
+const moveLeft = (event) => {
+  
+  if (event.keyCode == 37) {
+    movingRight = false;
+    movingLeft = true;
+    movingDown = false;
+    setInterval(movingBoxLeft, 1000);
+  }
+}
+
+
+document.addEventListener("keydown", moveRight);
+document.addEventListener("keydown", moveDown);
+document.addEventListener("keydown", moveLeft);
+
+
+
+
+
+
+
+
+
 
 
 
